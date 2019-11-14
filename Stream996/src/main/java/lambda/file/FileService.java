7b9707332839dfdb9afa6696636c4b6b3df743b0
yaron <liyashuo11@gmail.com>
2019-11-14 23:59:09 +0800

    更新慕课网 告别996课程 4-7 传统方式资源关闭

--- Stream996/src/main/java/lambda/file/FileService.java
+++ Stream996/src/main/java/lambda/file/FileService.java
@@ -12,15 +12,15 @@ public class FileService {
     /**
      * 通过url获取本地文件内容，调用函数式接口处理
      * @param url
-     * @param fileConsumer
+     * @param fileConsumer 自定义的函数式接口
      */
     public void fileHandle(String url, FileConsumer fileConsumer) throws IOException {
-        //创建文件读取流，读取本地文件
+        //创建文件读取流，读取本地文件（IO流就是一层套一层..）
         BufferedReader bufferedReader = new BufferedReader(
                 new InputStreamReader(
                         new FileInputStream(url)));
 
-        //定义行变量（存储每一行），和内容的StringBuilder（存储整个文件）
+        //定义 行变量（存储每一行），和 内容的StringBuilder（存储整个文件）
         String line;
         StringBuilder stringBuilder = new StringBuilder();
 
@@ -31,7 +31,7 @@ public class FileService {
             stringBuilder.append(line + "\n");
         }
 
-        //调用函数式接口方法，将文件内容传递给Lambda表达式，实现业务逻辑
+        //调用函数式接口方法，将文件内容（一个字符串）传递给Lambda表达式，实现业务逻辑
         fileConsumer.fileHandler(stringBuilder.toString());
     }
 
--- Stream996/src/test/java/lambda/file/FileServiceTest.java
+++ Stream996/src/test/java/lambda/file/FileServiceTest.java
@@ -13,11 +13,12 @@ public class FileServiceTest {
 
         //url参数右键java文件copy path就可以了；第二个参数是一个函数式接口，可以写成lambda表达式
         //通过lambda表达式，打印文件内容
-        fileService.fileHandle("D:\\ideaProject\\mooc\\Stream996\\src" +
-                        "\\main\\java\\lambda\\file\\FileConsumer.java",
+        fileService.fileHandle("D:\\ideaProject\\mooc\\Stream996\\src\\test\\java" +
+                        "\\lambda\\file\\FileServiceTest.java",
                 fileContent -> {
                     System.out.println(fileContent);//先打印出来
                     System.out.println("文本长度为：" + fileContent.length());
+                    //当然还可以进行更复杂的操作，比如统计词频、检查错词什么的
                 });
     }
 }
